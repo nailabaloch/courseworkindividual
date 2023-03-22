92b9a2f3288d133bbe716f910faa1df53c44fbeb
naila baloch <naila_baloch11@hotmail.com>
2023-03-23 02:13:34 +0300

    update

---
+++ subject.js
@@ -0,0 +1,64 @@
+let  subjects=[{
+
+    lesson: "English course",
+    location: 'Dera',
+    price: "500 AED",
+    spaces: 5,
+    image: "img/english course.jpg"
+}, {
+    lesson: "Robotics",
+    location: 'Al barsha',
+    price: "1000 AED",
+    spaces: 5,
+    image: "img/robotics.jpg"
+}, {
+    lesson: "Calligraphy & Handwriting",
+    location: 'Mirdift',
+    price: "700 AED",
+    spaces: 5,
+    image: "img/calli and handwriting.jpg"
+}, {
+    lesson: "Coding & Programming",
+    location: 'Business Bay',
+    price: "1000 AED",
+    spaces: 5,
+    image: "img/coding and programming.jpg"
+}, {
+    lesson: "Water Sports",
+    location: 'Jumeirah',
+    price: "400 AED",
+    spaces: 5,
+    image: "img/water sports.png"
+}, {
+    lesson: "Basic Computer",
+    location: 'Bur Dubai',
+    price: "600 AED",
+    spaces: 5,
+    image: "img/basic compurter.png"
+}, {
+    lesson: "Fashion Designing",
+    location: 'Dubai Marina',
+    price: "800 AED",
+    spaces: 5,
+    image: "img/fashion designning.jpg"
+}, {
+    lesson: "Sculpture Making",
+    location: 'Al karama',
+    price: "400 AED",
+    spaces: 5,
+    image: "img/sculpture making.jpg"
+}, {
+    lesson: "Aerobics",
+    location: 'Dubai Creek',
+    price: "600 AED",
+    spaces: 5,
+    image: "img/aerobics.jpg"
+
+}, {
+    lesson: "Zumba",
+    location: 'Silicon Oasis',
+    price: "450 AED",
+    spaces: 5,
+    image: "img/zumba.jpg"
+
+}, ]
